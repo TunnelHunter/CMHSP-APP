fb8eaf956ab1764bd1c94212aaa7b92dea058896
yangbo <1071011167@qq.com>
2018-04-13 14:14:18 +0800

    前端项目搭建
    项目搭建，搭建基本页面

--- js/app.js
+++ js/app.js
@@ -1,12 +1,61 @@
-var app = angular.module('myApp',['ionic']);
+var app = angular.module('myApp', ['ionic']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
-   //路由
-   $urlRouterProvider.otherwise('home');
-   $stateProvider
-       .state('home',{
-           url:'/home',
-           templateUrl:'html/home.html',
-           controller:'homeCtrl'
-       })
+    //路由
+    $urlRouterProvider.otherwise("/tab/home");
+    $stateProvider
+        .state('tabs', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "html/tabs.html"
+        })
+        .state('tabs.home', {
+            url: "/home",
+            views: {
+                'home-tab': {
+                    templateUrl: "html/home.html",
+                    controller: 'HomeTabCtrl'
+                }
+            }
+        })
+        .state('tabs.facts', {
+            url: "/facts",
+            views: {
+                'home-tab': {
+                    templateUrl: "html/facts.html"
+                }
+            }
+        })
+        .state('tabs.facts2', {
+            url: "/facts2",
+            views: {
+                'home-tab': {
+                    templateUrl: "html/facts2.html"
+                }
+            }
+        })
+        .state('tabs.about', {
+            url: "/about",
+            views: {
+                'about-tab': {
+                    templateUrl: "html/about.html"
+                }
+            }
+        })
+        .state('tabs.navstack', {
+            url: "/navstack",
+            views: {
+                'about-tab': {
+                    templateUrl: "html/nav_stack.html"
+                }
+            }
+        })
+        .state('tabs.contact', {
+            url: "/contact",
+            views: {
+                'contact-tab': {
+                    templateUrl: "html/contact.html"
+                }
+            }
+        });
 });
