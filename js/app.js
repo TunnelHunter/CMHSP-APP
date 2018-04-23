4cb557fd1d6ba10458598979f4be1edc588f973a
yangbo <1071011167@qq.com>
2018-04-23 17:58:59 +0800

    add test history records
    增加查看测试记录功能

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
 var app = angular.module('myApp', ['ionic']);
 // 这个地方的config不能少哦, 不然安卓平台的tabs会跑到顶部的
-app.config(['$ionicConfigProvider', function($ionicConfigProvider) {
+app.config(['$ionicConfigProvider', function ($ionicConfigProvider) {
 
     $ionicConfigProvider.tabs.position('bottom'); // other values: top
 
 }]);
 
-app.config(['$stateProvider','$urlRouterProvider',function ($stateProvider, $urlRouterProvider) {
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
     //路由
-    $urlRouterProvider.otherwise("/tab/test");
+    $urlRouterProvider.otherwise("/slides");
     $stateProvider
+        .state('slides', {
+            url: "/slides",
+            templateUrl: "html/hello_slides.html"
+
+        })
         .state('tabs', {
-            url: "/tab",
+            url: "/tabs",
             abstract: true,
             templateUrl: "html/tabs.html"
         })
@@ -19,10 +24,10 @@ app.config(['$stateProvider','$urlRouterProvider',function ($stateProvider, $url
             url: "/test",
             views: {
                 'test-tab': {
-                    templateUrl: "html/tab_test.html",
-                    controller: 'tabTestCtrl'
+                    templateUrl: "html/tab_test.html"
                 }
-            }
+            },
+            controller: 'tabTestCtrl'
         })
         .state('tabs.startAnswer', {
             url: "/startAnswer",
@@ -31,6 +36,17 @@ app.config(['$stateProvider','$urlRouterProvider',function ($stateProvider, $url
                     templateUrl: "html/tab_test_startAnswer.html"
                 }
             }
+        })
+        .state('tabs.historyRecords', {
+            url: "/historyRecodes",
+            views: {
+                'test-tab': {
+                    templateUrl: "html/tab_test_historyRecords.html"
+
+                }
+            },
+
+            controller: 'tabTestHistoryRecordsCtrl'
         })
         .state('tabs.social', {
             url: "/social",
