2f4ed8c014f3d04df07bc5032becb2139d6654d0
yangbo <1071011167@qq.com>
2018-05-02 18:12:28 +0800

    更新目录
    更新目录

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 //导入工具包 require('node_modules里对应模块')
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    cssmin = require('gulp-minify-css'),
+    cssmin = require('gulp-minify-css'),//css压缩  目前此插件已被废弃，改用gulp-clean-css插件
     gutil = require('gulp-util'),
     uglify = require('gulp-uglify'),//js压缩
     watchPath = require('gulp-watch-path'),
     combiner = require('stream-combiner2'),
     sourcemaps = require('gulp-sourcemaps'),
-    minifycss = require('gulp-minify-css'), //css压缩  目前此插件已被废弃，改用gulp-clean-css插件
     autoprefixer = require('gulp-autoprefixer'),
     sass = require('gulp-ruby-sass'),
     imagemin = require('gulp-imagemin'),
@@ -23,24 +22,24 @@ var SRC_DIR = './src/**/*.js';
 var DIST_DIR = './dist/';
 
 //定义一个testLess任务
-gulp.task('testLess', function () {
+gulp.task('lessToCss', function () {
     gulp.src('src/less/index.less')
         .pipe(less())
         .pipe(gulp.dest('src/css'));
 
     //编译src目录下的所有less文件
     //除了reset.less和test.less（**匹配src/less的0个或多个子文件夹）
-    gulp.src(['src/less/*.less', '!src/less/**/{reset,test}.less'])
-        .pipe(less())
-        .pipe(cssmin()) //兼容IE7及以下需设置compatibility属性 .pipe(cssmin({compatibility: 'ie7'}))
-        .pipe(gulp.dest('src/css'));
+    // gulp.src(['src/less/*.less', '!src/less/**/{reset,test}.less'])
+    //     .pipe(less())
+    //     .pipe(cssmin()) //兼容IE7及以下需设置compatibility属性 .pipe(cssmin({compatibility: 'ie7'}))
+    //     .pipe(gulp.dest('src/css'));
 });
 
-//当所有less文件发生改变时，调用testLess任务
-gulp.task('testWatch', function () {
-    gulp.watch('src/**/*.less', ['testLess']);
+//当所有less文件发生改变时，调用lessToCss任务
+gulp.task('watchLessToCss', function () {
+    gulp.watch('src/less/*.less', ['lessToCss']);
 });
 
 
 //默认任务
-gulp.task('default', ['testLess']);
+gulp.task('default', ['lessToCss','watchLessToCss']);
