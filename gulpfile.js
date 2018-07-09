517e9d55d78948a71a4ec9fcfd9d40d14c12e2fa
Mountain C44 <1071011167@qq.com>
2018-07-09 18:03:10 +0800

    gulp整理

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-//导入工具包 require('node_modules里对应模块')
+/**
+ * 导入工具包 require('node_modules里对应模块')
+ */
 var gulp = require('gulp'),
     less = require('gulp-less'),
     cssmin = require('gulp-clean-css'),//css压缩  目前此插件已被废弃，改用gulp-clean-css插件
@@ -26,7 +28,10 @@ var DIST_DIR = './dist/';
 
 
 
-
+/**
+ * 错误输出信息
+ * @param err
+ */
 var handleError = function (err) {
     var colors = gutil.colors;
     console.log('\n');
@@ -36,7 +41,79 @@ var handleError = function (err) {
     gutil.log('message: ' + err.message);
     gutil.log('plugin: ' + colors.yellow(err.plugin));
 };
-gulp.task('watchjs', function () {
+
+/**
+ * 把less文件转换为css文件，并监控指定路径下的less文件，一变化就调用lessToCss任务
+ */
+gulp.task('lessToCss', function () {
+    gulp.src('src/less/index.less')
+        .pipe(sourcemaps.init())
+        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+        .pipe(less())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('src/css'));
+
+    //编译src目录下的所有less文件
+    //除了reset.less和test.less（**匹配src/less的0个或多个子文件夹）
+    // gulp.src(['src/less/*.less', '!src/less/**/{reset,test}.less'])
+    //     .pipe(less())
+    //     .pipe(cssmin()) //兼容IE7及以下需设置compatibility属性 .pipe(cssmin({compatibility: 'ie7'}))
+    //     .pipe(gulp.dest('src/css'));
+});
+gulp.task('watchLessToCss', function () {
+    gulp.watch('src/less/*.less', ['lessToCss']);
+});
+
+
+/**
+ * autoprefixer
+ */
+gulp.task('autoFx', function () {
+    gulp.src('src/css/index.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'Android >= 4.0'],
+            cascade: true, //是否美化属性值 默认：true 像这样：
+            //-webkit-transform: rotate(45deg);
+            //        transform: rotate(45deg);
+            remove: true //是否去掉不必要的前缀 默认：true
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+
+
+/**
+ * js文件合并，js文件压缩，监控指定目录下的js文件，一变化就执行压缩方法
+ */
+gulp.task('concat', function () {
+    gulp.src(['src/js/config/*.js', 'src/js/controller/*.js', 'src/js/service/*.js'])
+        .pipe(concat('cmhsp_concat.js'))//合并后的文件名
+        .pipe(gulp.dest('dist/js'));
+});
+gulp.task('jsMin', function () {
+    gulp.src('dist/js/cmhsp_concat.js') //多个文件以数组形式传入
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/cmhsp_concat'));
+
+    //
+    // //压缩src/js目录下的所有js文件
+    // //除了test1.js和test2.js（**匹配src/js的0个或多个子文件夹）
+    // gulp.src(['src/js/*.js', '!src/js/**/{test1,test2}.js'])
+    //     .pipe(uglify())
+    //     .pipe(gulp.dest('dist/js'));
+    //
+    //
+    // gulp.src(['src/js/*.js', '!src/js/**/{test1,test2}.js'])
+    //     .pipe(uglify({
+    //         //mangle: true,//类型：Boolean 默认：true 是否修改变量名
+    //         //mangle: {except: ['require' ,'exports' ,'module' ,'$']}//排除混淆关键字
+    //         mangle: true,//类型：Boolean 默认：true 是否修改变量名
+    //         compress: true,//类型：Boolean 默认：true 是否完全压缩
+    //         preserveComments: 'all' //保留所有注释
+    //     }))
+    //     .pipe(gulp.dest('dist/js'));
+});
+gulp.task('watchJs', function () {
     gulp.watch('src/js/**/*.js', function (event) {
         var paths = watchPath(event, 'src/', 'dist/');
         /*
@@ -63,76 +140,11 @@ gulp.task('watchjs', function () {
     })
 });
 
-//定义一个testLess任务
-gulp.task('lessToCss', function () {
-    gulp.src('src/less/index.less')
-        .pipe(sourcemaps.init())
-        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-        .pipe(less())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('src/css'));
 
-    //编译src目录下的所有less文件
-    //除了reset.less和test.less（**匹配src/less的0个或多个子文件夹）
-    // gulp.src(['src/less/*.less', '!src/less/**/{reset,test}.less'])
-    //     .pipe(less())
-    //     .pipe(cssmin()) //兼容IE7及以下需设置compatibility属性 .pipe(cssmin({compatibility: 'ie7'}))
-    //     .pipe(gulp.dest('src/css'));
-});
-
-//当所有less文件发生改变时，调用lessToCss任务
-gulp.task('watchLessToCss', function () {
-    gulp.watch('src/less/*.less', ['lessToCss']);
-});
-
-
-
-gulp.task('testAutoFx', function () {
-    gulp.src('src/css/index.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'Android >= 4.0'],
-            cascade: true, //是否美化属性值 默认：true 像这样：
-            //-webkit-transform: rotate(45deg);
-            //        transform: rotate(45deg);
-            remove:true //是否去掉不必要的前缀 默认：true
-        }))
-        .pipe(gulp.dest('dist/css'));
-});
-
-
-gulp.task('testConcat', function () {
-    gulp.src(['src/js/config/*.js','src/js/controller/*.js','src/js/service/*.js'])
-        .pipe(concat('cmhsp_concat.js'))//合并后的文件名
-        .pipe(gulp.dest('dist/js'));
-});
-
-
-gulp.task('jsmin', function () {
-    gulp.src('dist/js/cmhsp_concat.js') //多个文件以数组形式传入
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js/cmhsp_concat'));
-
-    //
-    // //压缩src/js目录下的所有js文件
-    // //除了test1.js和test2.js（**匹配src/js的0个或多个子文件夹）
-    // gulp.src(['src/js/*.js', '!src/js/**/{test1,test2}.js'])
-    //     .pipe(uglify())
-    //     .pipe(gulp.dest('dist/js'));
-    //
-    //
-    // gulp.src(['src/js/*.js', '!src/js/**/{test1,test2}.js'])
-    //     .pipe(uglify({
-    //         //mangle: true,//类型：Boolean 默认：true 是否修改变量名
-    //         //mangle: {except: ['require' ,'exports' ,'module' ,'$']}//排除混淆关键字
-    //         mangle: true,//类型：Boolean 默认：true 是否修改变量名
-    //         compress: true,//类型：Boolean 默认：true 是否完全压缩
-    //         preserveComments: 'all' //保留所有注释
-    //     }))
-    //     .pipe(gulp.dest('dist/js'));
-});
-
-
-gulp.task('testCssmin', function () {
+/**
+ * 压缩css文件
+ */
+gulp.task('cssMin', function () {
     gulp.src('src/css/*.css')
         .pipe(cssmin())
         .pipe(gulp.dest('dist/css'));
@@ -150,8 +162,10 @@ gulp.task('testCssmin', function () {
 });
 
 
-
-gulp.task('testImagemin', function () {
+/**
+ * 图片压缩
+ */
+gulp.task('imageMin', function () {
     gulp.src('src/img/*.{png,jpg,gif,ico}')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
@@ -177,9 +191,10 @@ gulp.task('testImagemin', function () {
 });
 
 
-
-
-gulp.task('testHtmlmin', function () {
+/**
+ * html文件压缩
+ */
+gulp.task('htmlMin', function () {
     var options = {
         removeComments: true,//清除HTML注释
         collapseWhitespace: true,//压缩HTML
@@ -195,5 +210,9 @@ gulp.task('testHtmlmin', function () {
         .pipe(gulp.dest('dist/html'));
 });
 
-//默认任务
-gulp.task('default', ['lessToCss','watchLessToCss']);
+
+
+/**
+ * 执行默认任务 gulp
+ */
+gulp.task('default', ['lessToCss', 'watchLessToCss']);
